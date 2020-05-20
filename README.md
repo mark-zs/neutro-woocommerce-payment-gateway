9388c299085f60c2adcb5f619742ead67dcdbeeb
Dung Nguyen <dungqt3@gmail.com>
2020-05-20 09:38:42 +0700

    docker + plugin setup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # neutro-woocommerce-payment-gateway
 Neutro WooCommerce payment gateway plugin
+
+# Development setup
+```
+docker-compose up -d
+```
+
+* Importing database:
+```
+docker exec -i neutro-woocommerce-payment-gateway_db_1 mysql -u neutro_woocommerce_payment_gateway -p1 neutro_woocommerce_payment_gateway < db.sql
+```
+For more details, check `./docker-compose.yml`
---
+++ wp-content/plugins/neutro-woocommerce-payment-gateway/neutro-woocommerce-payment-gateway.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Neutro WooCommerce payment gateway
+ * Plugin URI:        #
+ * Description:       #
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            #
+ * Author URI:        #
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       neutro-woocommerce-payment-gateway
+ * Domain Path:       /languages
+ */
+
+defined('ABSPATH') or die('');
