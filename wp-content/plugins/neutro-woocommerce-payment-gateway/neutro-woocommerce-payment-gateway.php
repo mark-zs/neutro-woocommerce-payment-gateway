c5e912274455a774a00ad6a4239a70d70f3d09c4
Daniel Schreiber <shryber@users.noreply.github.com>
2020-06-24 13:06:54 +0200

    Update what you see on WP plugin page

--- wp-content/plugins/neutro-woocommerce-payment-gateway/neutro-woocommerce-payment-gateway.php
+++ wp-content/plugins/neutro-woocommerce-payment-gateway/neutro-woocommerce-payment-gateway.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * Plugin Name:       Neutro WooCommerce payment gateway
- * Plugin URI:        #
- * Description:       #
+ * Plugin URI:        https://www.neutro.net/
+ * Description:       Neutro is an easy checkout option which has an impact on the world.
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
- * Author:            #
- * Author URI:        #
+ * Author:            Neutro Ltd
+ * Author URI:        https://www.neutro.net/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       neutro-woocommerce-payment-gateway
